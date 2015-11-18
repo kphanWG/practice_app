dcfb4b8194bbd5824367c50d9110462cb852932f
Kevin <anjikoru@gmail.com>
2015-11-18 12:30:25 -0500

    Sign up wa kansei shimashita

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,45 @@
 class UsersController < ApplicationController
+	before_action :load_user, only: [:show, :destroy]
   def new
+  	@user = User.new
   end
 
-  def create
-  	
+   def create
+    	@user = User.new(user_params)
+    if @user.save
+    	flash[:success] = "Irasshaimase, kono okama yarou!"
+		redirect_to @user
+	else
+      render 'new'
+    end
   end
 
   def show
-    @user = User.find(params[:id])
-    # debugger
   end
+
+  def destroy
+  	@user.destroy
+  	# redirect_to users_url 
+  end
+
+  def index
+  	@users = User.all
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+    def load_user
+    	@user = User.find(params[:id])
+    	
+    end
+
+
+
+
 
 end
