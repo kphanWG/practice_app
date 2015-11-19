1b4bacd69e4a4af6a04d94a7816fe266a79f3c30
Kevin <anjikoru@gmail.com>
2015-11-19 14:05:46 -0500

    add passwood rests and snail confrontation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119165230) do
+ActiveRecord::Schema.define(version: 20151119184317) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20151119165230) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
