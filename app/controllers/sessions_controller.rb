41eb11382012d6675fc52ae2a72d97f47068951b
Kevin <anjikoru@gmail.com>
2015-11-19 10:46:07 -0500

    Log innings and outtings done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  # def log_in(user)
+  	
+  # end
+
+  def create
+  	    user = User.find_by(email: params[:session][:email].downcase)
+	    if user && user.authenticate(params[:session][:password])
+	    	log_in user
+	    	params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+	    	redirect_to user
+		else
+			flash.now[:danger] = 'Boss, what happened?!'
+  			render 'new'
+  		end
+  end
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def destroy
+  	log_out if logged_in?
+  	redirect_to root_url
+  end
+end
