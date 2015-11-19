3c7c68e66a1e9599ac78d17de0c19b8a5e678f48
Kevin <anjikoru@gmail.com>
2015-11-19 13:39:19 -0500

    added accouconut activia

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,17 +7,24 @@ class SessionsController < ApplicationController
   	
   # end
 
-  def create
+  	def create
   	    user = User.find_by(email: params[:session][:email].downcase)
 	    if user && user.authenticate(params[:session][:password])
-	    	log_in user
-	    	params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-	    	redirect_back_or user
-		else
+      		if user.activated?
+       		 log_in user
+        	 params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        	 redirect_back_or user
+      		else
+       		 message  = "Account not activated. "
+       		 message += "Check your email for the activation link."
+       		 flash[:warning] = message
+       		 redirect_to root_url
+      		end
+      	else
 			flash.now[:danger] = 'Boss, what happened?!'
   			render 'new'
   		end
-  end
+  	end
 
   def current_user
     User.find_by(id: session[:user_id])
