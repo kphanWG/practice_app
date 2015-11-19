8e61ebe0ffea21f11873fd2fdf0d7fffce05cbcc
Kevin <anjikoru@gmail.com>
2015-11-19 11:44:19 -0500

    Finnish user edit/update/index/destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
 	before_action :load_user, only: [:show, :destroy]
+  	before_action :logged_in_user, only: [:index, :edit, :update]
+  	before_action :correct_user,   only: [:edit, :update]
+
   def new
   	@user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
    def create
@@ -39,8 +56,18 @@ class UsersController < ApplicationController
     	
     end
 
+    def logged_in_user
+      unless logged_in?
+      	store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 
-
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
+    end
 
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,6 +18,10 @@ module ApplicationHelper
 		session[:user_id] = user.id 
 	end
 
+  def current_user?(user)
+    user == current_user
+  end
+
 	def current_user
     	if (user_id = session[:user_id])
       		@current_user ||= User.find_by(id: user_id)
@@ -45,4 +49,14 @@ module ApplicationHelper
     	session.delete(:user_id)
     	@current_user = nil
   	end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+   def store_location
+    session[:forwarding_url] = request.url if request.get?
+   end
+
 end
